b8d630e40f9dc4bd4e6153bee2a3adb6b03affeb
Ossama Zaid <w4rz4zi@gmail.com>
2024-04-22 20:12:12 +0100

    axum hello world

--- ingest/src/main.rs
+++ ingest/src/main.rs
@@ -1,4 +1,13 @@
-fn main() {
-  let module = "Ingest";
-  println!("Hello, {module}!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+  let router = Router::new().route("/", get(handler));
+  let listener = tokio::net::TcpListener::bind("0.0.0.0:3003").await.unwrap();
+  println!("listening on {}", listener.local_addr().unwrap());
+  axum::serve(listener, router).await.unwrap();
+}
+
+async fn handler() -> &'static str {
+  "Hello World"
 }
